d2817d04393a4a87b08753ec4713c29b33d39443
Tomas Langhi <tomas-langhi>
2020-10-30 15:08:08 -0300

    correcciones de idioma plantilla inicio sesion

--- PATAS/TeamAdmin/views.py
+++ PATAS/TeamAdmin/views.py
@@ -10,15 +10,15 @@ def signup_view(request):
     if form.is_valid():
         user = form.save()
         user.refresh_from_db()
-        user.profile.first_name = form.cleaned_data.get('first_name')
-        user.profile.last_name = form.cleaned_data.get('last_name')
+        user.profile.nombre = form.cleaned_data.get('nombre')
+        user.profile.apellido = form.cleaned_data.get('apellido')
         user.profile.email = form.cleaned_data.get('email')
         user.save()
         username = form.cleaned_data.get('username')
         password = form.cleaned_data.get('password1')
         user = authenticate(username=username, password=password)
         login(request, user)
-        return redirect('home')
+        return redirect('')
     else:
         form = SignUpForm()
     return render(request, "TeamAdmin/signup_view.html", {'form': form})
