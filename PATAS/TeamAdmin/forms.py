2c68e14a65397565ba678715285147efe891eea7
Tomas Langhi <tomas-langhi>
2020-10-31 17:11:32 -0300

    se agrego el parametro rol al registro de usuario

--- PATAS/TeamAdmin/forms.py
+++ PATAS/TeamAdmin/forms.py
@@ -2,12 +2,18 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+options = [
+    ('entrenador', 'Entrenador'),
+    ('jugador', 'Jugador'),
+]
+
 class SignUpForm(UserCreationForm):
     nombre = forms.CharField(max_length=100, help_text='Nombre')
     apellido = forms.CharField(max_length=100, help_text='Apellido')
     email = forms.EmailField(max_length=150, help_text='Email')
+    rol = forms.ChoiceField(required=True, widget = forms.Select, choices= options,)
 
     class Meta:
         model = User
         fields = ('username', 'nombre', 'apellido',
-                  'email', 'password1', 'password2',)
+                  'email', 'rol', 'password1', 'password2',)
